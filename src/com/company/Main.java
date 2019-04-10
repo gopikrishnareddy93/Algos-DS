f842dc96864964a56904079d74187e61aef9d8aa
Gopikrishna <gopi.jeevan97@gmail.com>
2019-04-10 06:37:56 -0500

    Inital commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.Algos.CountTotalSetBits;
+import com.company.Algos.HighestPowerOf2LessThanGivenNumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        CountTotalSetBits.Exercise.Run(6);
+//        HighestPowerOf2LessThanGivenNumber.Exercise.Run(10);
+    }
+}
