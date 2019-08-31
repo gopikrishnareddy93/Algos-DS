4a20defe97d1d1221eac76ba2acfcbfb7a7db05f
Gopikrishna Puchakayala <gpuchakayala@allen.achdirect.com>
2019-08-31 11:49:04 -0500

    Latest

--- src/repo/Algos/BinarySearch/BinarySearch.java
+++ src/repo/Algos/BinarySearch/BinarySearch.java
@@ -6,6 +6,7 @@ public class BinarySearch {
         public static void Run(){
             searchForAnTargetElement();
             searchForAnElementClosestToTheTarget();
+            searchForAnElementInARotatedArray();
         }
 
         static void searchForAnTargetElement(){
@@ -41,11 +42,10 @@ public class BinarySearch {
                 mid = (l + r)/2;
 
                 if (array[mid] == target){
-                    System.out.println(String.format("Found the target element %d at index = %d", target, mid));
+                    l = mid;
                     break;
                 }
                 else if (array[mid] < target){
-
                     l = mid + 1;
                 }
                 else {
@@ -57,9 +57,38 @@ public class BinarySearch {
         }
 
         static void searchForAnElementInARotatedArray(){
+            int[] array = new int[]{10,15,25,50,2,4,7};
+            int target = 4;
+            int l = 0, r = array.length - 1;
+            int mid;
 
+            //first find the index of the smallest element
+            while (l < r){
+                mid = (l + r) / 2;
+                if (array[mid] > array[r]){
+                    l = mid + 1;
+                }else {
+                    r = mid - 1;
+                }
+            }
+
+            int rotIndex = l,realmid;
+            l = 0; r = array.length - 1;
+
+            while (l <= r){
+                mid = (l + r) /2;
+                realmid = (mid + rotIndex)%array.length;
+                if (array[realmid] == target){
+                    System.out.println(String.format("Found the target element %d at index: %d", target, realmid));
+                    break;
+                }
+                else if (array[realmid] > target){
+                    r = mid - 1;
+                }
+                else {
+                    l = mid + 1;
+                }
+            }
         }
-
-
     }
 }
